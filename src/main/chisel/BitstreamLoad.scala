e60b6b63b4b945256a451a8fc26a64b58bfc831d
rbkettlewell <rbkettlewell@gmail.com>
2016-02-07 01:30:51 -0500

    Implemented HIP External/Internal connection

--- src/main/chisel/BitstreamLoad.scala
+++ src/main/chisel/BitstreamLoad.scala
@@ -12,12 +12,16 @@ class BitstreamLoad(bitFile : String){
   private val cols = xTiles*2 + 3
   private val rows = yTiles*2 + 3
   private val BlockSize = 72
+  private val Debug = true
 
   
   def getBlkBits(locationXY : (Int,Int)): UInt ={
     val row = locationXY._1
     val col = locationXY._2
     val readOffset = BlockSize*(cols*row + col)
+
+    if(Debug && (locationXY == (0,14)))
+      println(bitstream.slice(readOffset, readOffset + BlockSize).reverse.mkString)
     Cat(bitstream.slice(readOffset, readOffset + BlockSize).reverse.map(bit => UInt(bit.toString.toInt)))
   }
 }
--- src/main/chisel/TopLevel.scala
+++ src/main/chisel/TopLevel.scala
@@ -15,26 +15,38 @@ class TopLevel extends Module{
   val fpgaUtils = new FPGAUtils() 
   var fpga = fpgaUtils.assembleFPGA()
 
+  //Default Assignments
+  io.hipo:= UInt(0)
+  io.gpo := UInt(0)
+
+  // Connect HIP to external world
+  (1 to 8).foreach{i=> 
+    val iopad = fpga(0)(i*2).asInstanceOf[IOpad].io  
+      io.hipo(8-i) := iopad.outside.p0
+      iopad.outside.p1 := io.hipi(8-i)
+  }
+
   //below code works with tester
   val IHCB1 = fpga(2)(13).asInstanceOf[IHCB].io
   //val CLB1 = Module(new CLB())
   val CLB1 = fpga(2)(12).asInstanceOf[CLB].io
-
-  io.gpo := UInt(0)
   CLB1.E <> IHCB1.W
   IHCB1.S.p0 := io.gpi(0)
-  io.gpo(1) := CLB1.S.p6
+  fpga(0)(14).asInstanceOf[IOpad].io.inside.p0 := CLB1.S.p6
 }
 
 class TopLevelTest(c: TopLevel) extends Tester(c) {
   //poke(c.io.globalBlkBits(10)(11), int(UInt("h00_0000_0000_0000_0010")))
   //poke(c.io.globalBlkBits(10)(10), int(UInt("h03_0000_0000_0000_0006")))
   poke(c.io.gpi, 0)
-  expect(c.io.gpo, 0)
+  expect(c.io.hipo, 0)
   step(1)
   poke(c.io.gpi, 1)
-  expect(c.io.gpo, 2)
   step(1)
+  expect(c.io.hipo, 2)
+  peek(c.io.hipo)
+  step(1)
+  peek(c.io.hipo)
 }
 
 object TopLevel {
