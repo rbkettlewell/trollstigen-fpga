e03cb649e58f0bfc9fad85c6beb8b3e26414360c
rbkettlewell <rbkettlewell@gmail.com>
2016-02-07 00:44:14 -0500

    Fix the IOpads file directions

--- src/main/chisel/IOpad.scala
+++ src/main/chisel/IOpad.scala
@@ -4,48 +4,35 @@ import Chisel._
 
 class IOpad extends Module {
   val io = new Bundle {
-    val outside = new IOEdge()
+    val outside = new IOEdge().flip
     val inside = new IOEdge()
-    //val in_outside = UInt(INPUT, 1)
-    //val in_inside = UInt(INPUT, 1)
-    //val out_outside = UInt(OUTPUT, 1)
-    //val out_inside = UInt(OUTPUT, 1)
     val blkBits = UInt(INPUT,72)
   }
 
-  val dff1 = Reg(next = io.outside.p0)
+  val dffInside  = Reg(next = io.inside.p0)
+  val dffOutside = Reg(next = io.outside.p1)
 
-  val dff2 = Reg(next = io.inside.p0)
-
-  io.inside.p1 := dff1 & io.blkBits(0)
-
-  io.outside.p1 := dff2 & io.blkBits(1)
-
-
+  io.outside.p0:= dffInside & io.blkBits(0)
+  io.inside.p1 := dffOutside & io.blkBits(1)
 }
+
 class IOpadTests(c: IOpad) extends Tester(c) {
-  poke(c.io.blkBits, int(UInt("h0000_0000_0000_0000_0001"))) // select in_outside
-  poke(c.io.outside.p0, 1)
+  poke(c.io.blkBits, int(UInt("h0000_0000_0000_0000_0002"))) // External -> FPGA 
+  poke(c.io.outside.p1, 1)
   step(1)
   expect(c.io.inside.p1, 1)
   poke(c.io.inside.p0, 1)
   step(1)
-  expect(c.io.outside.p1, 0)
-  poke(c.io.outside.p0, 0)
-  step(1)
-  expect(c.io.inside.p1, 0)
-
-  poke(c.io.blkBits, int(UInt("h0000_0000_0000_0000_0002"))) // select in_inside
-  poke(c.io.outside.p0, 1)
+  expect(c.io.outside.p0, 0)
+  poke(c.io.blkBits, int(UInt("h0000_0000_0000_0000_0001"))) // FPGA -> External 
+  poke(c.io.outside.p1, 1)
   step(1)
   expect(c.io.inside.p1, 0)
   poke(c.io.inside.p0, 1)
   step(1)
-  expect(c.io.outside.p1, 1)
-  poke(c.io.outside.p0, 1)
-  step(1)
-  expect(c.io.inside.p1, 0)
+  expect(c.io.inside.p0, 1)
 }
+
 object IOpad{
   def main(args: Array[String]): Unit = {
     val tutArgs = args.slice(1, args.length)
