f3bc107b467fd3638314e7e435f6b08e147ba063
harrisonliew <harrisonliew@gmail.com>
2016-02-12 19:19:46 -0500

    SRAMTopLevel updated <C++ OOM error>

--- src/main/chisel/SRAMTopLevel.scala
+++ src/main/chisel/SRAMTopLevel.scala
@@ -14,20 +14,21 @@ class SRAMTopLevel extends Module {
     val cDataExtOut = UInt(OUTPUT,8)
   }
 
-  private val xTiles = 8
-  private val yTiles = 16
+  private val xTiles = 1
+  private val yTiles = 1
   private val cols = xTiles*2 + 3
   private val rows = yTiles*2 + 3
 
   val rowDecoder = Module(new RowDecoder())
   val columnDecoder = Module(new ColumnDecoder())
   val SRAMArray = Vec.fill(rows){Vec.fill(cols){Module(new SRAMBlock()).io}}
+
   for (i <- 0 until rows) {
     for (j <- 0 until cols) {
       SRAMArray(i)(j).bRow := rowDecoder.io.gRows(i)
       SRAMArray(i)(j).bRead := columnDecoder.io.bRead(j)
       SRAMArray(i)(j).bWrite := columnDecoder.io.bWrite(j)
-      SRAMArray(i)(j).bitRead := columnDecoder.io.byteRead(j)
+      columnDecoder.io.byteRead(j) := SRAMArray(i)(j).bitRead
       SRAMArray(i)(j).bitWrite := columnDecoder.io.byteWrite(j)
     }
   }
@@ -36,23 +37,25 @@ class SRAMTopLevel extends Module {
   val rasReg = Reg(UInt(0, 6))
   val basReg = Reg(UInt(0, 4))
 
+  io.cDataExtOut := UInt(0)
+  columnDecoder.io.WE := ~io.WEN
+  columnDecoder.io.RE := ~io.REN
+  columnDecoder.io.cDataIn := UInt(0)
+
   when (io.OEN === UInt(1)) {
-    columnDecoder.io.WE := ~io.WEN
-    columnDecoder.io.RE := ~io.REN
     when (io.CASN === UInt(0)) {
-      casReg := io.cDataExtIn(0,4)
+      casReg := io.cDataExtIn(4,0)
     } .elsewhen (io.RASN === UInt(0)) {
-      rasReg := io.cDataExtIn(0,5)
+      rasReg := io.cDataExtIn(5,0)
     } .elsewhen (io.BASN === UInt(0)) {
-      basReg := io.cDataExtIn(0,3)
+      basReg := io.cDataExtIn(3,0)
     }
     when (io.WEN === UInt(0)) {
       columnDecoder.io.cDataIn := io.cDataExtIn
     }
+  } .elsewhen (io.REN === UInt(0)) {
+      io.cDataExtOut := columnDecoder.io.cDataOut
   } .otherwise {
-      when (io.REN === UInt(0)) {
-        io.cDataExtOut := columnDecoder.io.cDataOut
-      }
   }
 
   columnDecoder.io.cAddr := casReg
@@ -61,32 +64,59 @@ class SRAMTopLevel extends Module {
 }
 
 class SRAMTopLevelTests(c: SRAMTopLevel) extends Tester(c) {
-/*  poke(c.io.cAddr, 0)
-  poke(c.io.WE, 1)
-  poke(c.io.RE, 0)
-  poke(c.io.cDataIn, 1)
-  expect(c.io.bWrite, 1)
-  expect(c.io.bRead, 0)
-  (0 until 19).foreach{i=>
-    if (i == 0)
-      expect(c.io.byteWrite(i), 1)
-    else{
-      expect(c.io.byteWrite(i), 0)
-    }
-  }
-  poke(c.io.WE, 0)
-  poke(c.io.RE, 1)
-  expect(c.io.bWrite, 0)
-  expect(c.io.bRead, 1)
-  expect(c.io.byteWrite(0), 0)
-  (0 until 19).foreach{i=>
-    if (i == 0)
-      poke(c.io.byteRead(i), 1)
-    else {
-      poke(c.io.byteRead(i), 0)
-    }
-  }
-  expect(c.io.cDataOut, 1)*/
+  poke(c.io.BASN, 1)
+  poke(c.io.RASN, 1)
+  poke(c.io.CASN, 1)
+  poke(c.io.OEN, 1)
+  poke(c.io.WEN, 1)
+  poke(c.io.REN, 1)
+  step(1)
+  poke(c.io.cDataExtIn, 1)
+  step(1)
+  poke(c.io.BASN, 0)
+  step(1)
+  poke(c.io.BASN, 1)
+  step(1)
+  poke(c.io.cDataExtIn, 1)
+  step(1)
+  poke(c.io.RASN, 0)
+  step(1)
+  poke(c.io.RASN, 1)
+  step(1)
+  poke(c.io.cDataExtIn, 1)
+  step(1)
+  poke(c.io.CASN, 0)
+  step(1)
+  poke(c.io.CASN, 1)
+  step(1)
+  poke(c.io.cDataExtIn, 2)
+  step(1)
+  poke(c.io.WEN, 0)
+  step(5)
+  poke(c.io.WEN, 1)
+  step(1)
+  poke(c.io.REN, 0)
+  step(1)
+  poke(c.io.BASN, 0)
+  step(1)
+  poke(c.io.BASN, 1)
+  step(1)
+  poke(c.io.cDataExtIn, 1)
+  step(1)
+  poke(c.io.RASN, 0)
+  step(1)
+  poke(c.io.RASN, 1)
+  step(1)
+  poke(c.io.cDataExtIn, 1)
+  step(1)
+  poke(c.io.CASN, 0)
+  step(1)
+  poke(c.io.CASN, 1)
+  step(1)
+  poke(c.io.REN, 0)
+  step(5)
+  poke(c.io.OEN, 0)
+  expect(c.io.cDataExtOut, 2)
 }
 
 object SRAMTopLevel{
